f6b21c837ee1e9c45c112a0d2e3a605d3b21ebb5
kesavan <arunprakash7871@gmail.com>
2023-02-28 03:07:17 -0500

    added javascript file for add input

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
-var guessEl = document.querySelector("#output-result");
-
+let guessEl = document.querySelector("#output-result");
+let guessValue= document.querySelector("#guessValue");
+let highScoreEl = document.querySelector("#highScore");
+let resetButton = document.querySelector("#reset");
+var guess= 7;
+var highScore=0;
 const result = Math.trunc((Math.random()*100)-1);
 console.log(result)
 
 function play(){
     let user_guess = document.querySelector("#guess").value;
-    if(user_guess <1 && user_guess > 100){
+    if(user_guess <1 || user_guess > 100){
         alert("Enter correct Value");
     }else{
-        if(user_guess < result){
+        
+        if(guess <= 0){
+        resetButton.style.display = 'block';
+        guessValue.textContent = "Guess:" + guess;
+        guessEl.textContent = "GameOver";
+       }else{
+        guess = guess - 1;
+        if(user_guess == result){
+            highScore = guess;
+            guessEl.textContent = "you won";
+            resetButton.style.display = 'block';
+            highScoreEl.textContent = "HighScore" + highScore;
+        }
+        else if(user_guess < result){
+
+            guessValue.textContent = "Guess:" + guess;
             guessEl.textContent = "your value is low"
         }
         else if(user_guess > result){
+            guessValue.textContent = "Guess:" + guess;
             guessEl.textContent = `your value is high ${user_guess}`
         }
-        else if(user_guess == result){
-            guessEl.textContent = "you won"
-        }
+       }
+        
     }
 }
